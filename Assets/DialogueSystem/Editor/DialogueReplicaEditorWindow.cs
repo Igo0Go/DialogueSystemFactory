356074409cd0cc01bf7ba6236af525d4e92460df
Игорь Чудакин <Chudakin98@gmail.com>
2022-03-06 11:17:19 +0700

    Многострочный текст в окне редакторе реплики

--- Assets/DialogueSystem/Editor/DialogueReplicaEditorWindow.cs
+++ Assets/DialogueSystem/Editor/DialogueReplicaEditorWindow.cs
@@ -9,6 +9,8 @@ public class DialogueReplicaEditorWindow : EditorWindow
     public DialogueSceneKit kit;
     private Vector2 verticalScrollPosition;
 
+    private GUIStyle style;
+
     public static DialogueReplicaEditorWindow GetReplicaWindow(ReplicInfo replica, DialogueSceneKit sceneKit)
     {
         var window = GetWindow<DialogueReplicaEditorWindow>();
@@ -24,11 +26,20 @@ public class DialogueReplicaEditorWindow : EditorWindow
 
     private void DrawReplica()
     {
+        if(style == null)
+        {
+            style = new GUIStyle(new GUISkin().textArea);
+            style.normal.textColor = Color.gray;
+            style.hover.textColor = Color.white;
+            style.focused.textColor = Color.white;
+            style.wordWrap = true;
+        }
+
         EditorGUILayout.BeginVertical();
         verticalScrollPosition = EditorGUILayout.BeginScrollView(verticalScrollPosition);
 
         replica.character = (DialogueCharacter)EditorGUILayout.ObjectField(replica.character, typeof(DialogueCharacter), allowSceneObjects: true);
-        replica.replicaText = EditorGUILayout.TextArea(replica.replicaText, GUILayout.MinHeight(50), GUILayout.MaxWidth(position.width - 10));
+        replica.replicaText = EditorGUILayout.TextArea(replica.replicaText, style, GUILayout.MinHeight(50), GUILayout.MaxWidth(position.width - 10));
         replica.clip = (AudioClip)EditorGUILayout.ObjectField(replica.clip, typeof(AudioClip), allowSceneObjects: true);
         replica.animType = (DialogueAnimType)EditorGUILayout.EnumPopup(replica.animType, GUILayout.MinWidth(80), GUILayout.MinHeight(20));
 
