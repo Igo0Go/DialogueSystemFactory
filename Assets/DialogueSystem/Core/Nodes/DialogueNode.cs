1f2d83115256a377cb55702d4c9371b456a89da8
Игорь Чудакин <Chudakin98@gmail.com>
2022-05-17 21:19:56 +0700

    Базовый класс для диалогового узла

---
+++ Assets/DialogueSystem/Core/Nodes/DialogueNode.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueNode
+{
+    #region Поля и свойства
+
+    /// <summary>
+    /// Номер в схеме диалога
+    /// </summary>
+    public int index;
+
+    /// <summary>
+    /// Индексы предыдущих узлов
+    /// </summary>
+    public List<int> previousNodesNumbers;
+
+    /// <summary>
+    /// Индексы следующих узлов
+    /// </summary>
+    public List<int> nextNodesNumbers;
+
+    /// <summary>
+    /// Этот узел заканчивает диалог/группу
+    /// </summary>
+    public bool finalNode;
+
+    #endregion
+
+    #region Конструкторы
+    /// <summary>
+    /// Создать дилоговый узел с указанным индексом в указанной позиции
+    /// </summary>
+    /// <param name="pos">позиция узла в координатах схемы</param>
+    /// <param name="index">индекс узла в схеме</param>
+    public DialogueNode(Vector2 pos, int index)
+    {
+        this.index = index;
+        previousNodesNumbers = new List<int>();
+        nextNodesNumbers = new List<int>();
+    }
+    #endregion
+
+    #region Методы
+
+    /// <summary>
+    /// Удалить указанный узел из списка следующих
+    /// </summary>
+    public void RemoveThisNodeFromNext(DialogueNode nodeForRemoving)
+    {
+        nextNodesNumbers.RemoveAll(item => item == nodeForRemoving.index);
+    }
+
+    /// <summary>
+    /// Удалить указанный узел из списка предыдущих
+    /// </summary>
+    /// <param name="nodeForRemoving">удаляемый узел</param>
+    public void RemoveThisNodeFromPrevious(DialogueNode nodeForRemoving)
+    {
+        previousNodesNumbers.RemoveAll(item => item == nodeForRemoving.index);
+    }
+
+    /// <summary>
+    /// корректировка номеров узлов после удаления
+    /// </summary>
+    /// <param name="removedIndex">индекс только что удалённого узла</param>
+    public void CheckIndexes(int removedIndex)
+    {
+        if (index > removedIndex)
+        {
+            index--;
+        }
+        for (int i = 0; i < previousNodesNumbers.Count; i++)
+        {
+            if (previousNodesNumbers[i] > removedIndex)
+            {
+                previousNodesNumbers[i]--;
+            }
+        }
+        for (int i = 0; i < nextNodesNumbers.Count; i++)
+        {
+            if (nextNodesNumbers[i] > removedIndex)
+            {
+                nextNodesNumbers[i]--;
+            }
+        }
+    }
+    #endregion
+}
